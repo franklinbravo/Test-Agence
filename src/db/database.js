582d3aa9da8966bbc567991970a820929247d85a
Franklin Bravo <51028566+franklinbravo@users.noreply.github.com>
2020-05-13 21:54:52 -0400

    Update database.js

--- src/db/database.js
+++ src/db/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const mysqlConnection = mysql.createPool({
-  host: 'us-cdbr-east-06.cleardb.net',
-  user: 'b76713da097d12',
-  password: '4384f445',
-  database: 'heroku_930f7921567023e',
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'banco_de_datos',
   connectionLimit: 10,
 })
 
